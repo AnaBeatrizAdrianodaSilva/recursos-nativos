421d81819a8f140362366c262a38b1740933881b
Ana Beatriz <ana.github0@gmail.com>
2023-06-12 14:07:46 -0300

    .

--- screens/Notify.jsx
+++ screens/Notify.jsx
@@ -2,6 +2,7 @@ import { View, Text, StyleSheet, Button } from "react-native";
 import Header from "../components/Header";
 import * as Notifications from "expo-notifications";
 import * as Battery from "expo-battery";
+import * as Device  from "expo-device";
 import { useEffect, useState } from "react";
 
 export default function DeviceInfo() {
@@ -14,6 +15,18 @@ export default function DeviceInfo() {
   }
 
   async function notificarExpo() {
+    const token = await Notifications.scheduleNotificationAsync({
+      content: {
+        title: "Notificação",
+        subtitle: "Subtitle",
+        body: "AAAAAAAAAAAAAAAAAAAAAAAAA",
+      },
+      trigger: { seconds: 1 },
+    });
+    setExpoToken(token);
+  }
+
+  async function notificarExpoBattery() {
     await bateria();
     const token = await Notifications.scheduleNotificationAsync({
       content: {
@@ -21,10 +34,34 @@ export default function DeviceInfo() {
         subtitle: "Subtitle",
         body: "Nivel é: " + battery +"%",
       },
-      trigger: { seconds: 3 },
+      trigger: { seconds: 1 },
     });
     setExpoToken(token);
   }
+
+  async function notificarExpoDeviceName() {
+    await bateria();
+    const token = await Notifications.scheduleNotificationAsync({
+      content: {
+        title: "Nome do Aparelho",
+        subtitle: "Subtitle",
+        body: "Nome do aparelho " + Device.osName,
+      },
+      trigger: { seconds: 1 },
+    });
+    setExpoToken(token);
+  }
+
+  const ultimaNotificacao = Notifications.useLastNotificationResponse();
+
+  async function exibirAlerta() {
+    alert('oi')
+    console.log(ultimaNotificacao);
+  }
+
+  useEffect(() => {
+    exibirAlerta();
+  }, [ultimaNotificacao])
 
   return (
     <View style={styles.container}>
@@ -38,7 +75,9 @@ export default function DeviceInfo() {
         />
         <Button title="Ler última notificação clicada" />
         <Button title="Ler notificações não clicadas" />
-        <Button title="Mostrar Bateria" onPress={async () => notificarExpo()} />
+        <Button title="Mostrar Bateria" onPress={async () => notificarExpoBattery()} />
+
+        <Button title="Nome do Aparelho" onPress={async () => notificarExpoDeviceName()} />
       </View>
     </View>
   );
