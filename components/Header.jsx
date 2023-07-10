65e1dc4fc57c7eb1900cfaed6fef04f9015d1b1f
Ana Beatriz <ana.github0@gmail.com>
2023-07-10 13:35:46 -0300

    header muda de cor está pronto

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,9 +1,36 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Button } from "react-native";
+import * as Battery from "expo-battery";
+import { useState } from "react";
+import { useEffect } from "react";
 
 export default function Header({ title }) {
+
+  const [nivelBateria, setNivelBateria] = useState();
+
+  async function atualizarTudo() {
+    bateria();
+  }
+
+  async function bateria() {
+    const nivel = await Battery.getBatteryLevelAsync();
+    setNivelBateria(Math.round(nivel * 100));
+  }
+
+  useEffect(()=> {
+    bateria()
+  }, [])
+
   return (
-    <View style={styles.header}>
+    <View style={{
+      backgroundColor: nivelBateria <= 100 && nivelBateria >= 80 ? 'green' : nivelBateria <= 79 && nivelBateria >= 50 ? 'yellow' : nivelBateria <= 49 && nivelBateria >= 30 ? 'orange' : 'red',
+      fontWeight: 'bolder'
+    }} >
       <Text style={styles.headerTextStyle}>{title}</Text>
+
+      <Button
+        title="Atualizar"
+        onPress={atualizarTudo}
+      />
     </View>
   );
 }
