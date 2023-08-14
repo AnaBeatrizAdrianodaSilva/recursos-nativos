d8093d1ff126d228e6cebd10981da6a837d30a11
Ana Beatriz <ana.github0@gmail.com>
2023-08-14 15:37:06 -0300

    DEU CERTOOOOOOOOOO

--- screens/Sensors.jsx
+++ screens/Sensors.jsx
@@ -1,83 +1,107 @@
-import { View, StyleSheet } from "react-native";
-import { Text } from "react-native-paper";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
-import { useState } from "react";
-
 import { Gyroscope, Magnetometer } from "expo-sensors";
-import { useEffect } from "react";
-
-export default function Sensor() {
-  const [giroscopio, setGiroscopio] = useState({});
-  const [magneto, setMagneto] = useState({});
-
-  useEffect(() => {
-    Gyroscope.addListener(giroscopioListner);
-    Magnetometer.addListener(magnetoListner);
-
-    // return () => {
-    //   Gyroscope.removeAllListeners();
-    //   Magnetometer.removeAllListeners();
-    // };
-  }, []);
-
-  const giroscopioListner = (data) => {
-    setGiroscopio(data);
-  };
-
-  const magnetoListner = (data) => {
-    setMagneto(data);
-  };
-
-  return (
-    <View style={styles.container}>
-      <Header title="Sensor" />
-
-      <View style={styles.container}>
-        <Text style={styles.sensor}>
-          Giroscopio: {"\n"}
-          x: {giroscopio.x} {"\n"}
-          y: {giroscopio.y} {"\n"}
-          z: {giroscopio.z} {"\n"}
-        </Text>
-
-        <Text>
-          Magneto: {"\n"}
-          x: {magneto.x} {"\n"}
-          y: {magneto.y} {"\n"}
-          z: {magneto.z} {"\n"}
-        </Text>
-      </View>
-
-      <Footer />
-    </View>
-  );
-}
+import { useEffect, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Header from "../components/Header";
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    gap: 10,
-  },
-  content: {
-    flex: 1,
-    gap: 20,
-    padding: 20,
-    alignSelf: "center",
-  },
-  contentTextStyle: {
-    padding: 5,
-    textAlignVertical: "center",
-    minHeight: 50,
-    backgroundColor: "#969",
-    color: "white",
-    fontWeight: "bold",
-    fontSize: 18,
-    textAlign: "center",
-  },
-  footer: {
-    backgroundColor: "#888",
-    paddingHorizontal: 25,
-    padding: 20,
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#c4beca",
+    },
+    title: {
+        fontSize: 35,
+        backgroundColor: '#949',
+        // fontWeight: 'bold',
+        color: '#fff',
+        marginBottom: 10,
+        padding: 10,
+        textAlign: 'center'
+    },
+
+    info: {
+        fontSize: 14,
+        backgroundColor: '#5e4d85',
+        color: '#fff',
+        marginBottom: 10,
+        padding: 10,
+        textAlign: 'center'
+    }
 });
+
+export default function SensorScreen() {
+
+    const [giroscopio, setGiroscopio] = useState({});
+    const [magneto, setMagneto] = useState({});
+    const [coro, setCoro] = useState('gray');
+    const [orientacao, setOrientacao] = useState('');
+
+    useEffect(() => {
+        Gyroscope.addListener(giroscopioListner);
+        Magnetometer.addListener(magnetoListner)
+    }, [])
+
+    const giroscopioListner = (data) => {
+        setGiroscopio(data);
+    }
+
+    const magnetoListner = (data) => {
+        setMagneto(data);
+    }
+
+
+    useEffect(() => {
+        if (magneto.x == -5.875 && magneto.y == 0 && magneto.z == -48.375) {
+            setCoro('red');
+            setOrientacao('Deitado a direita');
+
+        } else if (magneto.x == 5.875 && magneto.y == 0 && magneto.z == -48.375) {
+            setCoro('green');
+            setOrientacao('Deitado a esquerda');
+
+        } else if (magneto.y > 0.5) {
+            setCoro('blue');
+            setOrientacao('Vertical Up');
+
+        } else if (magneto.y < -0.5) {
+            setCoro('yellow');
+            setOrientacao('Vertical Down');
+
+            // }   else if (magneto.x > -0.625 && magneto.x < -3.8125){
+            //     return 'Inclinado Right';  
+            // }   else if (magneto.x > 0.625 && magneto.x < 3.625){
+            //     return 'Inclinado Left';
+        } else {
+            setOrientacao('Ué?');
+        }
+    }, [giroscopio, magneto])
+
+    return (
+        <View style={styles.container}>
+            <Header title="SENSOR" />
+            <View style={{
+                flex: 1,
+                backgroundColor: coro,
+            }}>
+                <Text style={{ fontSize: 18 }}>
+                    Giroscopio: {'\n'}
+                    x: {giroscopio.x} {'\n'}
+                    y: {giroscopio.y} {'\n'}
+                    z: {giroscopio.z} {'\n'}
+                </Text>
+                <Text style={{ fontSize: 18 }}>
+                    Magnetrometro: {'\n'}
+                    x: {magneto.x} {'\n'}
+                    y: {magneto.y} {'\n'}
+                    z: {magneto.z} {'\n'}
+                </Text>
+
+                <Text style={{
+                    fontSize: 18,
+                }}>
+                    Orientação: {orientacao}
+                </Text>
+
+            </View>
+        </View>
+    )
+}
